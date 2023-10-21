df00af8d91626f9ed6a83f0e8fbefb719f03b922
Ярослав Андреев <1frost34eone@gmail.com>
2023-10-21 15:14:55 +0300

    added model user

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -146,3 +146,5 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.User'
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Модель для пользователей."""
+
+    password = models.CharField(
+        verbose_name="Password",
+        max_length=150,
+    )
+
+    email = models.EmailField(
+        verbose_name="Адрес электронной почты", unique=True, max_length=254
+    )
+
+    class Meta:
+        ordering = ("id",)
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.username
