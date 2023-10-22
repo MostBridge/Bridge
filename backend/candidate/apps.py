8890cd56cbae8a7ec0561400435235f13960597b
Alexandr Slastukhin <111763438+last-ui@users.noreply.github.com>
2023-10-22 14:51:09 +0700

    add workflows ruff, tests, deploy (#15)
    * add workflows ruff, tests, deploy
    
    * add workflows ruff, tests, deploy

--- backend/candidate/apps.py
+++ backend/candidate/apps.py
@@ -2,6 +2,8 @@ from django.apps import AppConfig
 
 
 class CandidateConfig(AppConfig):
+    """AppConfig приложения candidate."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "candidate"
     verbose_name = "Кандидаты"
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -8,7 +7,7 @@ from rest_framework import permissions
 schema_view = get_schema_view(
     openapi.Info(
         title="Candidate API",
-        default_version='v1',
+        default_version="v1",
         description="Документация для проекта Bridge",
     ),
     public=True,
@@ -21,10 +20,19 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-    path('swagger<format>/',
-         schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
-         name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
-         name='schema-redoc'),
+    path(
+        "swagger<format>/",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
