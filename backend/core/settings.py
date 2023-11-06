6e300fc8a661b7c4defc15ac39d0f25d976f45cd
Ярослав Андреев <1frost34eone@gmail.com>
2023-11-06 14:56:08 +0300

    added domen

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -15,7 +15,7 @@ DEFAULT = "some_default_key"
 
 SECRET_KEY = env.str("SECRET_KEY", default=DEFAULT)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=['*', ".vercel.app"])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=['*', "bridge-henna.vercel.app"])
 
 DEFAULT_APPS = [
     "django.contrib.admin",
